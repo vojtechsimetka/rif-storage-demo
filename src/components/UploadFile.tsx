3675e0cf18a3477fdf4243ecb9c17107acb03cb1
Vojtech Simetka <vojtech@simetka.cz>
2020-04-22 16:13:09 +0200

    feat: removed provider, upload button is disabled when uploading

--- src/components/UploadFile.tsx
+++ src/components/UploadFile.tsx
@@ -3,22 +3,22 @@ import { Table, Button } from "react-bootstrap";
 import { useDropzone } from "react-dropzone";
 import UploadProvider, { EPROVIDER_TYPE } from "providers/UploadProvider";
 import Provider from "components/Provider";
-import {IFile} from 'types'
+import { IFile } from "types";
 
 export default () => {
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
   const [hash, setHash] = useState();
+  const [uploading, setUploading] = useState();
 
   return (
     <UploadProvider.Consumer>
-      {({ actions: { upload } ,state: {provider} }) => (
+      {({ actions: { upload }, state: { provider } }) => (
         <section className="upload-file-container">
-          <Provider />
+          {/*<Provider />*/}
           <div {...getRootProps({ className: "dropzone mb-4" })}>
             <input {...getInputProps()} />
             <span>Drag 'n' drop some files here, or click to select files</span>
           </div>
-
           {acceptedFiles.length > 0 && (
             <div>
               <Table striped borderless responsive size="sm">
@@ -30,8 +30,8 @@ export default () => {
                   </tr>
                 </thead>
                 <tbody className="fs-11">
-                  {console.log({acceptedFiles})}
-                  {acceptedFiles.map((file: File)=> (
+                  {console.log({ acceptedFiles })}
+                  {acceptedFiles.map((file: File) => (
                     <tr key={file.name}>
                       <td className="text-left">{(file as IFile).path}</td>
                       <td className="text-center">{file.type}</td>
@@ -41,28 +41,49 @@ export default () => {
                 </tbody>
               </Table>
               <div className="text-right">
-                <Button
-                  variant="secondary"
-                  size="lg"
-                  onClick={async () => {
-                    const hash = await upload(acceptedFiles);
-                    setHash(hash);
-                  }}
-                >
-                  Upload
-                </Button>
+                {!hash && (
+                  <Button
+                    variant="secondary"
+                    size="lg"
+                    disabled={uploading}
+                    onClick={async () => {
+                      setUploading(true);
+                      const hash = await upload(acceptedFiles);
+                      setHash(hash);
+                      setUploading(false);
+                    }}
+                  >
+                    {uploading ? "Uploading" : "Upload"}
+                  </Button>
+                )}
               </div>
             </div>
           )}
-
           {hash && (
             <div>
               Your file was successfully uploaded and is accessible under this
               hash:
               <br />
-              <pre>{provider === EPROVIDER_TYPE.IPFS && "/ipfs/"}{hash}</pre>
-              {provider === EPROVIDER_TYPE.IPFS && <a className="btn btn-lg btn-secondary" target="blank" href={`https://ipfs.io/ipfs/${hash}`}>View upload</a>}
-              <a className="btn btn-lg btn-primary" target="blank" href={`https://twitter.com/intent/tweet/?status=See my latest article on RIF Storage. ${encodeURIComponent("https://ipfs.io/ipfs//"+hash)}&url=${provider === EPROVIDER_TYPE.IPFS && encodeURIComponent("https://ipfs.io/ipfs//"+hash)}`}>Tweet!</a>
+              <pre>{hash}</pre>
+              {provider === EPROVIDER_TYPE.IPFS && (
+                <a
+                  className="btn btn-lg btn-secondary"
+                  target="blank"
+                  href={`https://ipfs.io/ipfs/${hash}`}
+                >
+                  View upload
+                </a>
+              )}
+              <a
+                className="btn btn-lg btn-primary"
+                target="blank"
+                href={`https://twitter.com/intent/tweet/?status=See my latest article on RIF Storage. ${encodeURIComponent(
+                  "https://ipfs.io/ipfs//" + hash
+                )}&url=${provider === EPROVIDER_TYPE.IPFS &&
+                  encodeURIComponent("https://ipfs.io/ipfs//" + hash)}`}
+              >
+                Tweet!
+              </a>
             </div>
           )}
         </section>
